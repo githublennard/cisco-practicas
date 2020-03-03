ce533e588c0aad0e4cb092cebf4a0a02b7dc2e6f
lennard <lennardwilde@gmail.com>
2020-03-03 01:16:25 +0100

    python file example

---
+++ getRoute.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import urllib3
+
+from pprint import pprint
+
+
+
+
+requests.packages.urllib3.disable_warnings()
+
+url= "http://www.mapquestapi.com/directions/v2/route?key=AGRVT98WynBY3tQur6DtGJM6I1JC4bB7&from=madrid&to=zaragoza"
+
+
+#Estableciendo las cabeceras// Que sea un diccionario
+headers = {
+
+    'Content-Type':'application/json'
+}
+
+#variable que almacena la respuesta de la peticion que se hace
+                        #cabecera   #en este parentesis indicamos todo lo que  lleva la peticion
+resp = requests.get(url,headers=headers,verify=False)
+
+#variable 
+print("La peticion tiene el estado",resp.status_code)
+
+#Esta es una variable que indica que la variable "resp" me la devuelva en formato "json" 
+response_json = resp.json()
+
+#print(response_json)
+
+#libreria para tener una vista mas limpia al capturar datos
+#pprint(response_json)
+
+#Aqui se parsea la respuesta, es decir devuelve lo que que te indico en los "[]"
+print("La distancia es:",response_json['route']['distance'],"km")
+
+
+
+
