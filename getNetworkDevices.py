14748cbd934e2b02f165ffc93760b76fdbd8b68a
lennard <lennardwilde@gmail.com>
2020-03-04 01:46:57 +0100

    Getting Data From NetworkDevices

--- getNetworkDevices.py
+++ getNetworkDevices.py
@@ -14,14 +14,14 @@ url= "https://devnetsbx-netacad-apicem-3.cisco.com/api/v1/network-device"
 #Estableciendo las cabeceras// Que sea un diccionario
 headers = {
 
-    'Content-Type':'application/json'
-    'X-Auth-Token' : 'ST-1892-YcftmbIKhqWbZzC6irsm-cas'
+    'Content-Type' : 'application/json',
+    'X-Auth-Token' : 'ST-1980-bt07o7vMKeSPG4evWGnH-cas'
 }
 
-body_json = {
-  "password": "Xj3BDqbU",
-  "username": "devnetuser"
-}
+#body_json = {
+ # "password": "Xj3BDqbU",
+  #"username": "devnetuser"
+#}
 
 #variable que almacena la respuesta de la peticion que se hace
                         #cabecera   #en este parentesis indicamos todo lo que  lleva la peticion
@@ -30,8 +30,10 @@ body_json = {
 #json.dumps --> Serializacion (Convertirlo a json) // Esto se hace ya que estoy usando python pero el endpoint tiene json
 #json.loads  --> .json  --> deserializar (Convertirlo a dictionary))
 
-resp = requests.post(url,json.dumps(body_json),headers=headers,verify=False)
+#resp = requests.post(url,json.dumps(body_json),headers=headers,verify=False)
 
+
+resp = requests.get(url,headers=headers,verify=False)
 
 #variable 
 print("La peticion tiene el estado",resp.status_code)
@@ -44,7 +46,7 @@ response_json = resp.json()
 #libreria para tener una vista mas limpia al capturar datos y verlos impreso en pantalla
 #pprint(response_json)
 
-print("El ticket de servicio es:",response_json['response']['serviceTicket'])
+#print("El ticket de servicio es:",response_json['response']['serviceTicket'])
 
 #Aqui se parsea la respuesta, es decir devuelve lo que que te indico en los "[]", se obtiene un resultado mas exacto
 #print("La distancia es:",response_json['route']['distance'],"km")
@@ -52,5 +54,18 @@ print("El ticket de servicio es:",response_json['response']['serviceTicket'])
 #Para usar esta opcion se hace un casting, se modifica el tipo de solicitud que se hace
 #pprint("La distancia es:" +str( response_json['route']['distance'])+"km")
 
+#El hostname, pertenece a la familia <familia>, y su MAC es <MAC> y su version de IOS es <version>
+#for equipo in response_json
 
+###
+#counter = 0
+#for equipo in response_json['response']:
+#    print("El hostname",response_json['response'][counter]['hostname'],"pertenece a la familia",response_json['response'][counter]['family'],
+#    "su MAC es", response_json['response'][counter]['macAddress'],"y su version de IOS es:",response_json['response'][counter]['softwareVersion'])
+#    counter +=1
+###  
 
+#equipo hace el papel de iterador
+for equipo in response_json['response']:
+    print("El hostname",equipo['hostname'],"pertenece a la familia",equipo['family'],
+    "su MAC es",equipo['macAddress'],"y su version de IOS es:",equipo['softwareVersion'])
