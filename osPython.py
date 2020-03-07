488f0606efd2561be807bf9c49d73b946d41dd2d
lennard <lennardwilde@gmail.com>
2020-03-07 23:33:04 +0100

    Examples Library Python

---
+++ osPython.py
@@ -0,0 +1,12 @@
+import os
+
+print ("Estamos en:",os.getcwd())
+
+
+print ("El user es:",os.environ["USER"])
+
+#USER es una palabra reservada
+
+print(dir(os))
+
+
---
+++ systemPython.py
@@ -0,0 +1,9 @@
+import sys
+
+print("El primer argumento",sys.argv[1])
+print("El segundo argumento",sys.argv[2])
+
+sys.exit("Salida Curso Marzo")
+
+
+#python3 -i systemPython.py "estudia" "ahora"  --->> "-i" es de interactivo 
