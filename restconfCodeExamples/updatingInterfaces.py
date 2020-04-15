091d06c4369088c4d52f55c481851e70db1d8815
lennard <lennardwilde@gmail.com>
2020-04-15 17:57:21 +0200

    getting individual interface

--- restconfCodeExamples/updatingInterfaces.py
+++ restconfCodeExamples/updatingInterfaces.py
@@ -19,35 +19,31 @@ basic_auth = ("cisco","cisco_1234!")
 
 #Info to send
 yangConfiguration = {
-    "ietf-interfaces:interfaces": {
-        "interface": [
-            {
-                "name": "Loopback99",
-                "description": "Interface_Prueba_Loopback",
-                "type": "iana-if-type:softwareLoopback",
-                "enabled": True,
-                "ietf-ip:ipv4": {
-                    "address": [
-                        {
-                            "ip": "99.99.99.99",
-                            "netmask": "255.255.255.0"
-                        }
-                    ]
-                },
-                "ietf-ip:ipv6": {}
-            }
-        ]
+    "ietf-interfaces:interface": {
+        "name": "Loopback99",
+        "description": "Interface_Prueba_Loopback",
+        "type": "iana-if-type:softwareLoopback",
+        "enabled": True,
+        "ietf-ip:ipv4": {
+            "address": [
+                {
+                    "ip": "99.99.99.99",
+                    "netmask": "255.255.255.0"
+                }
+            ]
+        },
+        "ietf-ip:ipv6": {}
     }
 }
 
-            #asi es que se hace la peticion con get
+            #asi es que se hace la peticion con put pero con la intencion de crear una interface en el servidor
 #Saving response            
 resp = requests.put(api_url, data=json.dumps(yangConfiguration) , auth=basic_auth, headers=headers ,verify=False)
 
 if(resp.status_code >= 200 and resp.status_code <= 299):
     print("Status OK:{}".format(resp.status_code))
 else:
-    print("Error code:{}, reply;{}".format(resp.status_code, resp.json()))
+    print("Error code {}, reply:{}".format(resp.status_code, resp.json()))
 
 #resp_json = resp.json()
 
